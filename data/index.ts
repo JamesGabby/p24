d2afa74fe1604be2574cd20379ea6f790b1f6eeb
JamesGabby <jlghfe36@gmail.com>
2024-08-30 13:16:14 +0100

    update links in footer

--- data/index.ts
+++ data/index.ts
@@ -196,13 +196,17 @@ export const socialMedia = [
   {
     id: 1,
     img: "/git.svg",
+    url: "https://github.com/jamesgabby",
   },
   {
     id: 2,
     img: "/twit.svg",
+    url: "",
   },
   {
     id: 3,
     img: "/link.svg",
+    url: "https://www.linkedin.com/in/james-gabbitus/",
+    
   },
 ];
