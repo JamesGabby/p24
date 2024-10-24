3acae3c1416aa56b65edafd4efa63ba56f530276
JamesGabby <jlghfe36@gmail.com>
2024-10-24 20:14:01 +0300

    amend text

--- data/index.ts
+++ data/index.ts
@@ -114,7 +114,7 @@ export const testimonials = [
   },
   {
     quote:
-      "Collaborating with James was an absolute pleasure. His professionalism, promptness, and dedication to delivering exceptional results were evident throughout our project. James's enthusiasm for every facet of development truly stands out. If you're seeking to elevate your website and elevate your brand, Adrian is the ideal partner.",
+      "Collaborating with James was an absolute pleasure. His professionalism, promptness, and dedication to delivering exceptional results were evident throughout our project. James's enthusiasm for every facet of development truly stands out. If you're seeking to elevate your website and elevate your brand, James is the ideal partner.",
     name: "Michael Johnson",
     title: "Director of AlphaStream Technologies",
     image: "profile.svg"
